a33b62814cd65b9816bbc76b1e596ff32da21268
Ben Fowlie <bsfowlie@gmail.com>
2019-12-23 01:27:34 -0500

    Create frame on Swing's EDT

--- src/main/java/com/github/bsfowlie/snake/Main.java
+++ src/main/java/com/github/bsfowlie/snake/Main.java
@@ -3,9 +3,22 @@
  */
 package com.github.bsfowlie.snake;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()->{
+            JFrame frame = new JFrame();
+
+            frame.setBounds(10, 10, 905, 700);
+            frame.setResizable(false);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        });
+
     }
 
 }
