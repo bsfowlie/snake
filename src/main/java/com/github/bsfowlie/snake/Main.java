052c8ae3e51a556c4279fa5302c6b9491ef4ee6e
Ben Fowlie <bsfowlie@gmail.com>
2019-12-22 23:35:50 -0500

    Run on the Swing Event Dispatch Thread

--- src/main/java/com/github/bsfowlie/snake/Main.java
+++ src/main/java/com/github/bsfowlie/snake/Main.java
@@ -10,15 +10,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame();
-        Gameplay game = new Gameplay();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            Gameplay game = new Gameplay();
 
-        frame.setBounds(10, 10, 905, 700);
-        frame.setBackground(Color.DARK_GRAY);
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(game);
+            frame.setBounds(10, 10, 905, 700);
+            frame.setBackground(Color.DARK_GRAY);
+            frame.setResizable(false);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(game);
+        });
 
     }
 
