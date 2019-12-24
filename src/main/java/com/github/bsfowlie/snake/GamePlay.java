91ca27886617e9ced05b07cb3db0cbf9958dfd6b
Ben Fowlie <bsfowlie@gmail.com>
2019-12-23 20:44:17 -0500

    Organize imports

--- src/main/java/com/github/bsfowlie/snake/GamePlay.java
+++ src/main/java/com/github/bsfowlie/snake/GamePlay.java
@@ -3,10 +3,7 @@ package com.github.bsfowlie.snake;
 import static java.util.Objects.requireNonNull;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.util.List;
 import java.util.*;
 import java.util.function.Function;
